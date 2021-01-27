17a74097f293c3ddaf1c0d76f0ade2e259d6a0d1
abmarie <77949813+abmarie@users.noreply.github.com>
2021-01-26 20:41:44 -0800

    Added quotes to array

--- js/script.js
+++ js/script.js
@@ -10,7 +10,86 @@ project 1 - A Random Quote Generator
 /***
  * `quotes` array
 ***/
+const quotes [
+  {
+    quote:"Like Beyoncé, I excel as a solo artist.",
+    citation:"David",
+    year:2018,
+    season:4,
+    episode:"Girls Night",
+  },
+  {
+    quote:"I'm trying very hard not to connect with people right now.",
+    citation:"David",
+    year:2015,
+    season:1,
+    episode:"The Drip",
+  },
+  {
+    quote:"Stop doing that with your face.",
+    citation:"Alexis",
+    year:2016,
+    season:2,
+    episode:"The Candidate",
+  },
+  {
+    quote:"I don't skate through life, David. I walk through life, in really nice shoes.",
+    citation:"Alexis",
+    year:2017,
+    season:3,
+    episode:"Driving Test",
+  },
+  {
+    quote:"I'm positively bedeviled with meetings et cetera.",
+    citation:"Moira",
+    year:2018,
+    season:4,
+    episode:"Dead Guy in Room 4",
+  },
+  {
+    quote:"I would be pleased to RSVP as pending.",
+    citation:"Moira",
+    year:2017,
+    season:3,
+    episode:"Murder Mystery",
+  },
+  {
+    quote:"I never heard someone say so many wrong things, one after the other, consecutively, in a row.",
+    citation:"David",
+    year:2018,
+    season:4,
+    episode:"Girls' Night",
+  },
+  {
+    quote:"We can talk about this anytime you'd like. Preferably not before 10am, because I'm not really a morning person.",
+    citation:"David",
+    year:2017,
+    season:3,
+    episode:"Grad Night",
+  },
+  {
+    quote:"David, stop acting like a disgruntled pelican.",
+    citation:"Moira",
+    year:2017,
+    season:3,
+    episode:"Opening Night",
+  },
+  {
+    quote:"Tweet us on Facebook!",
+    citation:"Johnny",
+    year:2017,
+    season:3,
+    episode:"Motel Review",
+  },
+  {
+    quote:"It's almost dark and my son is afraid of moths",
+    citation:"Johnny",
+    year:2015,
+    season:1,
+    episode:"Our Cup Runneth Over",
+  },
 
+]
 
 
 /***
